a2e039501c7a4e0fd85e2546cc12dceb12590f9f
kyleeautumnkim <kak012@ucsd.edu>
2025-05-29 00:00:38 -0700

    Fix default exports in about and project pages

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">About Me</h1>
+        <p className="mt-4">I am a UX/UI designer and cognitive science student!</p>
+      </main>
+    );
+  }
+  
